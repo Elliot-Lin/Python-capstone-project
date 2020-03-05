d9d1aad81af814c5b18036ffc6c6eb280e5cae7c
Elliot Lin <52466804+Elliot-Lin@users.noreply.github.com>
2020-03-05 12:13:30 -0500

    Add files via upload

---
+++ Python Capstone (1).py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook is primarily use for python capstone project
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+import numpy as np
+
+
+# In[5]:
+
+
+print(" Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
